e2c0559fc338f16e5ec36f2dad3bda26a4de10c4
Andy Ly <andywindsong@gmail.com>
2017-06-14 12:49:14 -0700

    let's try committing with this IDE

---
+++ ideaJava/HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by janly on 6/14/17.
+ * Trying out using a new IDE
+ */
+public class Hello {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
